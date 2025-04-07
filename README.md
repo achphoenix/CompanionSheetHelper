ab955e2d879ea29af252d6c178cfe8ed30f626ce
Alex Chris Haislund <alexhaislund@live.dk>
2025-04-07 22:43:21 +0200

    Added more stats with scripting. Updated design. Updated readme. Next up: Damage and Heal buttons.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Companion Sheet Helper
 ## Must haves
-- Display base attributes of companion (With bonuses from PC level, proficiency, etc, added and text "greened" to show the bonus at a glance.
+- ~~Display base attributes of companion (With bonuses from PC level, proficiency, etc, added and text "greened" to show the bonus at a glance.~~
 - ~~Dropdown to select PC's level. Default 1.~~
 - Display current **Ferocity** in an overrideable box.
 - Add a dropdown to select current number of adjecant enemies.
@@ -10,7 +10,7 @@
 - Clearly display what happens when a **RAMPAGE** occurs.
 - Clearly display WHEN **ferocity** is rolled. (Start of turn IF not incapacitated.)
 - Clickable skill buttons that show at a glance what they do, and will minus the required **ferocity** to use the skill when clicked.
-- List of Traits.
+- ~~List of Traits.~~
 - List of Bonus Actions.
 - Input boxes for healing and damage, like BDnD's.
 - Cookie to save current data and load it when opening site again.
@@ -24,3 +24,6 @@
 - Dice roll sound.
 - Health change text animations.
 - Animations on notification text on damage taken or dice rolled.
+- Cool font
+- Dropdown styling
+- Button styling
--- src/Companion_Helper.js
+++ src/Companion_Helper.js
@@ -3,38 +3,61 @@ var elements = {};
 
 // On Load
 addEventListener("DOMContentLoaded", () => {
-    DefaultData();
     FetchElements();
+    DefaultData();
     InitiateLabels();
+
+    document.getElementById("Everything").style.display = "block";
 
     elements.Player_Character_Level.addEventListener("change", (event) => {
         PlayerLevelChanged(event);
     });
 });
 
+function FetchElements()
+{
+    elements.Player_Character_Level = document.getElementById("Player_Character_Level");
+    elements.Max_Hit_Points_Label = document.getElementById("Max_Hit_Points_Label");
+    elements.Current_Health = document.getElementById("Current_Health");
+    elements.Armor_Class_Label = document.getElementById("Armor_Class_Label");
+    elements.Player_Proficiency = document.getElementById("Player_Proficiency");
+    elements.Saving_Throws_Bonus = document.getElementById("Saving_Throws_Bonus");
+    elements.Stealth_Skill_Bonus = document.getElementById("Stealth_Skill_Bonus");
+}
+
 function DefaultData() {
     // todo-ah: Get from cookie.
     data.Player_Level = 1;
     data.Player_Proficiency = 2;
     data.Max_Health = 7 + (7 * data.Player_Level);
+    data.Current_Health = data.Max_Health;
     data.Armor_Class = 13 + data.Player_Proficiency;
-}
-
-function FetchElements()
-{
-    elements.Player_Character_Level = document.getElementById("Player_Character_Level");
-    elements.Max_Hit_Points_Label = document.getElementById("Max_Hit_Points_Label");
-    elements.Armor_Class_Label = document.getElementById("Armor_Class_Label");
+    data.Player_Proficiency = (Math.ceil(data.Player_Level / 4) + 1);
+    data.Saving_Throws_Bonus = 2 + data.Player_Proficiency; // Dex(1) + 1 + Player Proficiency Bonus
+    data.Stealth_Skill_Bonus = 1 + data.Player_Proficiency;
 }
 
 function InitiateLabels() {
-    // Health
+    // Player Proficiency
+    elements.Player_Proficiency.textContent = data.Player_Proficiency;
+
+    // Max Health
     elements.Max_Hit_Points_Label.textContent = data.Max_Health;
     elements.Max_Hit_Points_Label.title = "7 + (7 * Player Level)";
+
+    // Health
+    elements.Current_Health.value = data.Current_Health;
 
     // Armor
     elements.Armor_Class_Label.textContent = data.Armor_Class;
     elements.Armor_Class_Label.title = "13 + PC Proficiency";
+
+    // Basic Traits
+    data.Saving_Throws_Bonus = 2 + data.Player_Proficiency; // Dex(1) + 1 + Player Proficiency Bonus
+    elements.Saving_Throws_Bonus.textContent = data.Saving_Throws_Bonus;
+
+    data.Stealth_Skill_Bonus = 1 + data.Player_Proficiency;
+    elements.Stealth_Skill_Bonus.textContent = data.Stealth_Skill_Bonus;
 }
 
 
@@ -42,15 +65,27 @@ function PlayerLevelChanged(event) {
     // Level
     data.Player_Level = event.target.value;
 
-    // Health
+    // Player Proficiency
+    data.Player_Proficiency = (Math.ceil(data.Player_Level / 4) + 1)
+    elements.Player_Proficiency.textContent = data.Player_Proficiency;
+
+    // Max Health
     data.Max_Health = 7 + (7 * data.Player_Level);
     elements.Max_Hit_Points_Label.textContent = data.Max_Health;
     elements.Max_Hit_Points_Label.title = "7 + (7 * Player Level)";
 
-    // Proficiency
-    
+    // Health
+    data.Current_Health = data.Max_Health;
+    elements.Current_Health.value = data.Max_Health;
 
     // Armor
     elements.Armor_Class_Label.textContent = data.Armor_Class;
     elements.Armor_Class_Label.title = "13 + PC Proficiency";
+
+    // Basic Traits
+    data.Saving_Throws_Bonus = 2 + data.Player_Proficiency; // Dex(1) + 1 + Player Proficiency Bonus
+    elements.Saving_Throws_Bonus.textContent = data.Saving_Throws_Bonus;
+
+    data.Stealth_Skill_Bonus = 1 + data.Player_Proficiency;
+    elements.Stealth_Skill_Bonus.textContent = data.Stealth_Skill_Bonus;
 }
